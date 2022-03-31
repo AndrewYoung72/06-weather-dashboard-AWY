d18efbe0afd97a58a9b80752800dd895aeabd166
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-03-31 09:01:25 -0600

    some readme and js cahanges to html

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Purpose
 
 For this assignment we are asked to build a weather dashboard utilizing server side APIs. The user should be able to see the weather outlook for multiple cities, which are stored to the side in a search history. When presented with the city of choice the user should see the date with an iconic representation of the weather and a UV index. The UV index should indicate by color wether the conditions are favorable, moderate, or severe. When the user clicks on a city in the search history then the stored five day forecast for that city should be presented.
+
+## Description
+
+file:///Users/andrewyoung/Desktop/Homework/06-weather-dashboard-AWY/index.html
--- assets/script.js
+++ assets/script.js
@@ -4,19 +4,19 @@ var cityArray = [];
 
 
 function getApi(){
-  var weatherUrl = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},ISO 3166-2:US&limit=1&appid=483a5f8a85f972bf48f65c53caa33848";
+  var weatherUrl = "http://api.openweathermap.org/geo/1.0/direct?q=Chicago,ISO 3166-2:US&limit=1&appid=483a5f8a85f972bf48f65c53caa33848";
 
   fetch(weatherUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-
+      console.log(data)
       for (var i = 0; i < data.length; i++) {
         var citySearch = document.getElementById("weather");
         var currentWeather = document.createElement("div");
-        var searchEl = $("<div>");
-        searchEl.textContent = data[i].html_url;
+        var searchEl = document.createElement("div");
+        searchEl.textContent = data[i];
         citySearch.append(searchEl);
       }
     });
