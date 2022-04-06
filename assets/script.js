bd7478fa3eb1af550598d2af6a18019e36ac2dfb
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-04-06 12:32:53 -0600

    some css js and html

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,14 @@
 
 var searchBtn = document.getElementById("searchBtn");
-
+var cityList = document.getElementById("searchList");
+var cityInput = document.getElementById("cSearch").value;
+var searchList = [];
 
 
 function getApi(){
   var cityInput = document.getElementById("cSearch").value;
   var geoCodeUrl = "https://api.openweathermap.org/geo/1.0/direct?q="+cityInput+"&limit=1&appid=483a5f8a85f972bf48f65c53caa33848";
+  
 
   fetch(geoCodeUrl)
     .then(function (response) {
@@ -15,7 +18,7 @@ function getApi(){
       var lat = data[0].lat;
       var lon = data[0].lon;
       console.log(data)
-      var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&units=imperial&appid=483a5f8a85f972bf48f65c53caa33848"
+      var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&units=imperial&appid=483a5f8a85f972bf48f65c53caa33848";
 
       fetch(weatherUrl)
       .then(function (response) {
@@ -28,8 +31,15 @@ function getApi(){
         var windEl = document.getElementById("wind");
         var humidEl = document.getElementById("humid");
         var uviEl = document.getElementById("uvI");
+        var listEl = document.getElementById("searchList");
+
+        searchBtn.addEventListener("click", function(event) {
+          event.preventDefault();
+          localStorage.setItem("searchList", JSON.stringify(data[0].name))
+        }, false);
 
         cityNameEl.textContent = data[0].name;
+        listEl.textContent = data[0].name;
         tempEl.textContent = "Temp: " + weatherData.current.temp;
         windEl.textContent = "Wind: " + weatherData.current.wind_speed;
         humidEl.textContent = "Humidity: " + weatherData.current.humidity;
@@ -45,10 +55,12 @@ function getApi(){
 
         var dateEl = document.getElementById("date");
         var iconEl = document.getElementById("icon");
+        var descriptionEl = document.getElementById("description")
         var tempCardEl = document.getElementById("tempCard");
         
         dateEl.textContent = weatherData.daily[0].dt;
-        iconEl.textContent = weatherData.daily[0].weather[0].description;
+        iconEl.src = weatherData.daily[0].weather[0].icon;
+        descriptionEl.textContent = weatherData.daily[0].weather[0].description;
         tempCardEl.textContent = "Temp: " + weatherData.daily[0].temp.day;
        
 
@@ -57,7 +69,7 @@ function getApi(){
         var nTempCardEl = document.getElementById("nTempCard");
        
         nDateEl.textContent = weatherData.daily[1].dt;
-        nIconEl.textContent =  weatherData.daily[1].weather[0].description;
+        nIconEl.src =  weatherData.daily[1].weather[0].icon;
         nTempCardEl.textContent = "Temp: " + weatherData.daily[1].temp.day;
        
 
@@ -66,7 +78,7 @@ function getApi(){
         var sTempCardEl = document.getElementById("sTempCard");
         
         sDateEl.textContent = weatherData.daily[2].dt;
-        sIconEl.textContent = weatherData.daily[2].weather[0].description;
+        sIconEl.src = weatherData.daily[2].weather[0].icon;
         sTempCardEl.textContent = "Temp: " + weatherData.daily[2].temp.day;
        
 
@@ -75,7 +87,7 @@ function getApi(){
         var tTempCardEl = document.getElementById("tTempCard");
         
         tDateEl.textContent = weatherData.daily[3].dt;
-        tIconEl.textContent = weatherData.daily[3].weather[0].description;
+        tIconEl.src = weatherData.daily[3].weather[0].icon;
         tTempCardEl.textContent = "Temp: " + weatherData.daily[3].temp.day;
        
 
@@ -84,13 +96,13 @@ function getApi(){
         var fTempCardEl = document.getElementById("fTempCard");
         
         fDateEl.textContent = weatherData.daily[4].dt;
-        fIconEl.textContent = weatherData.daily[4].weather[0].description;
+        fIconEl.src = weatherData.daily[4].weather[0].icon;
         fTempCardEl.textContent = "Temp: " + weatherData.daily[4].temp.day;
        
-
-
-
       })
+
+
+      
       // for (var i = 0; i < data.length; i++) {
       //   var citySearch = document.getElementById("searchList");
       //   var cityArray = [];
@@ -104,5 +116,17 @@ function getApi(){
   // var currentWeather = document.createElement("div");
 }
 
-// searchBtn.on("click", getApi());
+searchBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  var cityInput = document.getElementById("cSearch").value;
+
+
+
+});
+
+    var cityList = document.getElementById("searchList");
+    var listName = document.createElement("li");
+    listName.textContent = cityInput;
+  
+
 searchBtn.addEventListener("click", getApi);
