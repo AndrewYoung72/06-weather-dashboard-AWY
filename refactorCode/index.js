fc596e08b58b79879f58bee14d38f06ca4319866
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-09-21 12:50:30 -0600

    working on city search input

--- refactorCode/index.js
+++ refactorCode/index.js
@@ -1,2 +1,9 @@
+const searchInput = document.querySelectorAll(".input").value;
 const city = document.getElementById("city").value;
 const state = document.getElementById("state").value;
+
+document.querySelector(".input").addEventListener("submit", function(event) {
+  event.preventDefault();
+  console.log(searchInput);
+})
+
