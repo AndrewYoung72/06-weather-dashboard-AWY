e159b5b7b11200fe84c8fab360ace3778c1e3ca0
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-09-21 17:13:38 -0600

    geo locate call succesful and returning data

--- refactorCode/index.js
+++ refactorCode/index.js
@@ -21,3 +21,17 @@ document.querySelector("form.input").addEventListener("submit", function(event) 
 //   div.appendChild(document.createTextNode(city.value, state.value));
 //   document.getElementById("savedCities").appendChild(div);
 // }
+
+
+//Geo locate API call
+async function geoCodeUrl(city) {
+  try {
+    const city = document.getElementById("city").value;
+    var geoCodeUrl = "https://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=1&appid=483a5f8a85f972bf48f65c53caa33848";
+    let response = await fetch(geoCodeUrl);
+    return response.json();
+  } catch(err) {
+    console.log(err);
+  } 
+  console.log(response);
+}
