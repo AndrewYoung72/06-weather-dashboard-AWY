dfa288c05d7922c977a5548efad0fda553b4a5d6
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-09-21 15:37:44 -0600

    generating div and class for city list

--- refactorCode/index.js
+++ refactorCode/index.js
@@ -2,10 +2,19 @@ const city = document.getElementById("city");
 const state = document.getElementById("state");
 document.querySelector("form.input").addEventListener("submit", function(event) {
   event.preventDefault();
-  const insert = document.getElementById("footer");
+  const insert = document.getElementById("savedCities");
   const div = document.createElement('div');
   div.textContent = city.value +", "+ state.value;
+  div.classList.add("new");
   insert.appendChild(div);
   console.log(city.value, state.value);
 })
 
+
+// function addCity() {
+//   const city = document.getElementById("city");
+//   const state = document.getElementById("state");
+//   const div = document.createElement(div);
+//   div.appendChild(document.createTextNode(city.value, state.value));
+//   document.getElementById("savedCities").appendChild(div);
+// }
