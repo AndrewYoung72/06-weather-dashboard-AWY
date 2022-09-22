d437061538fd9eb3b594f974fb7b42f7dac3c416
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-09-22 11:29:41 -0600

    getting weather data/state input notworking yet

--- refactorCode/index.js
+++ refactorCode/index.js
@@ -10,7 +10,7 @@ document
     div.textContent = city.value + ", " + state.value;
     div.classList.add("new");
     insert.appendChild(div);
-    cityList.push(city.value +","+ state.value);
+    cityList.push(city.value + "," + state.value);
     console.log(cityList);
     city.value = "";
     state.value = "";
@@ -25,18 +25,47 @@ document
 // }
 
 //Geo locate API call
-async function getApi() {
-  try {
-    const city = document.getElementById("city").value;
-    var geoCodeUrl =
-      "https://api.openweathermap.org/geo/1.0/direct?q=" +
-      city +
-      "&limit=1&appid=483a5f8a85f972bf48f65c53caa33848";
-    let response = await fetch(geoCodeUrl);
-    console.log(response);
-    return response.json();
-  } catch (err) {
-    console.log(err);
-  } 
-}
+// async function getApi() {
+//   try {
+//     const city = document.getElementById("city").value;
+//     var geoCodeUrl =
+//       "https://api.openweathermap.org/geo/1.0/direct?q=" +
+//       city +
+//       "&limit=1&appid=483a5f8a85f972bf48f65c53caa33848";
+//     let response = await fetch(geoCodeUrl);
+//     console.log(response);
+//     return response.json();
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
 
+function getApi() {
+  const city = document.getElementById("city").value;
+  const state = document.getElementById("state").value;
+  const geoCodeUrl =
+    "https://api.openweathermap.org/geo/1.0/direct?q=" +
+    city + "," +state+
+    "&limit=1&appid=483a5f8a85f972bf48f65c53caa33848";
+    fetch(geoCodeUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        const lat = data[0].lat;
+        const lon = data[0].lon;
+        console.log(data);
+        const weatherUrl =    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&units=imperial&appid=483a5f8a85f972bf48f65c53caa33848";
+        fetch(weatherUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (weatherData) {
+          console.log(weatherData);
+        })
+      })
+}
